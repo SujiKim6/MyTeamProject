12b09c74af5ac31fbf63697853e59b94679aa4f8
heerim <heerim12@naver.com>
2017-07-28 16:14:22 +0900

    - test commit for android test

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+    id: 'com.example.test',
+    name: 'test',
+    description: 'Get über power in one button click',
+    author: 'Matt Development Group',
+    email: 'contact@example.com',
+    website: 'http://example.com'
+});
