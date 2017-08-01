2a85e7a1e27123a3ad23c96ec603abb06707a465
heerim <heerim12@naver.com>
2017-08-01 10:52:14 +0900

    - tab에서 프로젝트 관련 페이지 수정 작업 by희림, (2017.07.31)

--- client/addProject.js
+++ client/addProject.js
@@ -1,14 +1,29 @@
 Template.addProject.events({
+
+    //addProject의 프로젝트 할일 추가 + 버튼
+    'click #addProjectPlusBtn': function (evt, tmpl) {
+        var todo_list = $('#todoInput').val();
+
+        //할일을 할일DB에 추가한다.
+        todoDB.insert({
+            createdAt: new Date(),
+            project_id: this._id,
+            content: todo_list
+        });
+    },
+
+    //addProject의 확인 버튼
     'click #confirmBtn': function(evt, tmpl) {
         var project_name =  $('#projectName').val();
         var project_goal =  $('#projectGoal').val();
         var startDate =  $('#inpStartDate').val();
         var endDate =  $('#inpEndDate').val();
         var publicSelected = false;
+
         if(document.getElementById('public').checked) {
             publicSelected = true;
         }
-
+        //프로젝트DB에 넣는다.
         projectDB.insert({
             createdAt: new Date(),
             name: project_name,
@@ -19,4 +34,7 @@ Template.addProject.events({
             isPublic: publicSelected
         });
     }
-})
+
+
+
+});
