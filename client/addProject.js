65bc3a75a611efabc2c416a787ae39d1dfa8d8e0
Suji <rlatnwl9595@naver.com>
2017-08-01 14:11:27 +0900

    Suji update(17.08.01. 14:09)

--- client/addProject.js
+++ client/addProject.js
@@ -8,12 +8,15 @@ Template.addProject.events({
         if(document.getElementById('public').checked) {
             publicSelected = true;
         }
+        
+        // 빈칸을 모두 채우지 않을 경우 에러메세지 띄우고 돌아가기
         if((project_name === undefined )&&(project_goal ===undefined)&&(startDate===undefined) &&(endDate===undefined) )
         {
             //error 메세지 띄우기
-            return error;
+            return alert("모든 정보를 입력하세요.");
         }
         
+        // 빈칸을 모두 채웠을 경우 DB에 추가하기
         projectDB.insert({
             createdAt: new Date(),
             name: project_name,
--- client/projectMain.js
+++ client/projectMain.js
@@ -1,3 +1,8 @@
 Template.projectMain.helpers({
-
+    projects: function (evt, tmpl) {
+        // 사용자가 하고 있는 프로젝트가 하나라도 존재할 경우 하나씩 보내기
+        if(projectDB.find({username:username}).count()!==0){
+            return projectDB.find()
+        }
+    }
 })
