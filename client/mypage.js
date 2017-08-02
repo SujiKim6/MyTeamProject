2624bc8cc8cf667fe13adf398d098736b476ba99
Suji <rlatnwl9595@naver.com>
2017-08-02 18:43:02 +0900

    final3_suji&jaeeun 1st update(17.08.02. 18:42)

--- client/mypage.js
+++ client/mypage.js
@@ -13,19 +13,25 @@ Template.mypage.helpers({
         // 현재 사용자의 정보
         var loginedId = SessionStore.get('myEmail'); //로그인된 아이디
         var userInfo = userDB.find({username: loginedId}).fetch();
-        var name = userDB.find({name: loginedId}).fetch();
+        // var name = userDB.find({name: loginedId}).fetch();
         return userInfo;
+    },
+    // 변경 버튼을 눌렀을 때 UI 바꾸기
+    change:function () {
+        return Session.get('clickedChange');
     }
 });
 
 
 Template.mypage.events({
-    'click #confirm': function () {
+    //변경버튼을 누른 다음에 창이 바뀌고 확인버튼 눌렀을 때 변경이 되야한다구
+    'click #confirmChange': function () {
         var loginedId = $('#usernameInput').val();
         var newPassword = $('#passwordInput').val();
+        var newPasswordConfirm = $('#passwordInputConfirm').val();
         var newName = $('#nameInput').val();
-        var school = $('#schoolInput').val();
-        var phone = $('#phoneInput').val();
+        // var school = $('#schoolInput').val();
+        // var phone = $('#phoneInput').val();
 
         //필수 입력사항 빈칸 있으면 오류
         if((newPassword==="" ) || (newName==="")) {
@@ -33,18 +39,42 @@ Template.mypage.events({
             alert("비밀번호와 이름은 필수 입력사항 입니다.");
             return;
         }
-
-        if (confirm('회원정보를 변경하시겠습니까?')) {
-
-            //회원 업데이트
-            userDB.update({username: loginedId}, {
-                $set: {
-                    password: newPassword,
-                    name: newName,
-                }
-            });
-            location.href="/projectMain";
+        //패스워드와 패스워드 확인input이 틀리면 오류
+        if((newPassword !== newPasswordConfirm)) {
+            //error 메세지 띄우기
+            alert("비밀번호와 비밀번호 확인이 다릅니다.");
+            return;
         }
+        // alert(loginedId);
+        // alert(newPassword);
+        // alert(newName);
+        Meteor.call('updateUser', loginedId, newPassword, newName, function(err, rslt) {
+            // if(err) {
+            //     alert(err.toString());
+            // }
+            if(rslt.status === 'success') {
+                //성공
+                // alert('success');
+                if(confirm("변경이 완료되었습니다.")){
+                    Session.set('clickedChange',false);
+                    location.href='/proMain';
+                }
+            }
+
+        });
+        //회원 업데이트
+        // var result = userDB.update({username: loginedId}, {
+        //     $set: {
+        //         password: newPassword,
+        //         name: newName
+        //     }
+        // });
+        // alert(result);
+
+    },
+
+    'click #change':function () {
+        Session.set('clickedChange',true);
     }
 
 });
--- server/methods.js
+++ server/methods.js
@@ -1,4 +1,25 @@
 Meteor.methods({
+    // 마이페이지 변경 부분 server에서 처리하기
+    'updateUser': function(loginedId, newPassword, newName) {
+        var updateRslt = userDB.update({username: loginedId}, {
+            $set: {
+                password: newPassword,
+                name: newName
+            }
+        });
+        // user가 한명일 때 이렇게 되는거
+        if (updateRslt === 1) {
+            return {
+                status: 'success'
+            }
+        }
+        else {
+            return {
+                status: 'something goes wrong'
+            }
+        }
+
+    },
     'editTodo' :function(data) {
 
         todoDB.update({_id: data._id}, {
