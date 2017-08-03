abfee02183fa3f3159a43e6407a40d8d1d543b27
Suji <rlatnwl9595@naver.com>
2017-08-03 20:42:48 +0900

    진짜 파이널 17.08.03. 20:42

--- start.bat
+++ start.bat
@@ -1,11 +1,11 @@
 set MONGO_URL=mongodb://1.255.55.20:30000/teamhub
 
-meteor
+rem meteor
 
 rem 는 주석
 
 rem 아래는 ANDROID_HOME variable이 없다고 실행시에 에러가 날경우 등록 (자신의 안드로이드 sdk 디렉토리를 입력)
-rem set ANDROID_HOME=C:\SWUAndroid\sdk
+rem set ANDROID_HOME=C:\SDK\sdk
 rem set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
 
 rem 아래는 gradle 패키지가 sdk에 포함이 안되어 있다고에러가 날 경우 실시
@@ -18,7 +18,7 @@ rem 프로젝트에 안드로이드 플랫폼 개발환경 추가
 rem meteor add-platform android
 
 rem 안드로이드 에뮬레이터 실행
-rem meteor run android
+meteor run android
 
 rem 안드로이드 실제 단말 실행
 rem meteor run android-device
