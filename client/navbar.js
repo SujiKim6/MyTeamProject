dfa5ee098242d5f9d173d24f69e7513f2ac7ad99
HeerimYun <heerim12@naver.com>
2017-08-03 20:18:32 +0900

    second commit

--- client/navbar.js
+++ client/navbar.js
@@ -3,3 +3,45 @@ $(function() {
         $('.navbar-collapse').collapse('hide');
     });
 });
+
+Template.navbar.helpers({
+    user: function () {
+        var user = userDB.findOne({username: SessionStore.get('myEmail')});
+        return user.name
+    }
+});
+
+Template.navbar.events({
+    //teamhub 눌렀을 때 처리
+    'click #brandName':function () {
+        if(SessionStore.get('myEmail')==' ')
+        {
+            location.href="/";
+            return;
+        }
+        else
+        {
+            location.href="/proMain";
+            return;
+        }
+    },
+
+    //로그아웃 탭 눌렀을 때 처리
+    'click #signOut':function () {
+        if(confirm('정말 로그아웃 하시겠습니까?')) {
+            SessionStore.set('myEmail',' ');
+            location.href="/";
+        }
+        else {
+        }
+    },
+
+    //마이페이지 탭 눌렀을 때 처리
+    'click #myPage':function () {
+        if(SessionStore.get('myEmail')!=' ')
+        {
+            location.href="/mypage";
+            return;
+        }
+    }
+})
